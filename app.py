59cb4766b01ea00640f3a6a926bb1e76acadac22
Mustafa <khanzadi.3298@gmail.com>
2023-06-23 12:44:51 +0330

    f

--- app.py
+++ app.py
@@ -1,31 +1,42 @@
-from flask import Flask , render_template , jsonify
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
-JOBS = [
-    {'id': 1,
-     'title' : 'Data Analyst',
-     'location' : 'Tehran',
-     'salary': '$120,000'},
-    {'id': 2,
-     'title' : 'Data Scientist',
-     'location' : 'Yazd',
-     'salary': '$130,000'},    
-    {'id': 3,
-     'title' : 'Financial Analyst',
-     'location' : 'Tabriz',
-     'salary': '$140,000'},
-    {'id': 4,
-     'title' : 'Administrator',
-     'location' : 'Remote',
-     'salary': '$150,000'},
-]
-@app.route('/')
-def hello_world():
-    return render_template('home.html' , jobs =JOBS)
 
-@app.route('/jobs')
-def Jason_():
-    return jsonify(JOBS)
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Bengaluru, India',
+    'salary': 'Rs. 10,00,000'
+  },
+  {
+    'id': 2,
+    'title': 'Data Scientist',
+    'location': 'Delhi, India',
+    'salary': 'Rs. 15,00,000'
+  },
+  {
+    'id': 3,
+    'title': 'Frontend Engineer',
+    'location': 'Remote'
+  },
+  {
+    'id': 4,
+    'title': 'Backend Engineer',
+    'location': 'San Francisco, USA',
+    'salary': '$150,000'
+  }
+]
+
+@app.route("/")
+def hello_jovian():
+    return render_template('home.html', 
+                           jobs=JOBS, 
+                           company_name='Jovian')
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 if __name__ == '__main__':
-    app.run(host= '0.0.0.0', debug=True)
+  app.run(host='0.0.0.0', debug=True)
